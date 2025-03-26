c19fc25fafa03989152cd16eff21c8f2c9b9f093
RafaelaAfferri <rafaelaao@al.insper.edu.br>
2025-03-26 13:37:39 -0300

    exercico feito

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import requests
 import datetime
+from fastapi.requests import Request
 
 
 
@@ -11,16 +12,17 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/coins/currency1/currency2")
-def excahnge(currency1: str, currency2: str):
-    response = requests.get(f"https://economia.awesomeapi.com.br/json/last/:moedas/{currency1}-{currency2}")
+@app.get("/coins/{currency1}/{currency2}")
+def excahnge(request: Request ,currency1: str, currency2: str):
+    response = requests.get(f"https://economia.awesomeapi.com.br/json/last/{currency1}-{currency2}")
     compra = response.json()[f"{currency1}{currency2}"]["bid"]
     venda = response.json()[f"{currency1}{currency2}"]["ask"]
+    account = request.headers["id-account"]
     resposta = {
     "sell": venda,
     "buy": compra,
     "date": datetime.datetime.now() ,
-    "id-account": ""
+    "id-account": account
     }
     return resposta
     
